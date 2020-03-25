3a26d003181a4d64a4d2d2a72b493905dea2f795
Lucas <lucashen@gmail.com>
2020-03-25 18:46:01 -0300

    add incident delete route

--- src/routes.js
+++ src/routes.js
@@ -8,5 +8,6 @@ routes.post('/ongs', OngController.create);
 
 routes.get('/incidents', IncidentsController.index);
 routes.post('/incidents', IncidentsController.create);
+routes.delete('/incidents', IncidentsController.delete);
 
 module.exports = routes;
