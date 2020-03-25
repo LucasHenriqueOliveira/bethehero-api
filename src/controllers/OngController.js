a836053ee5caeb053c1428128bad97d90db7d150
Lucas <lucashen@gmail.com>
2020-03-25 18:25:35 -0300

    add incidents routes

--- src/controllers/OngController.js
+++ src/controllers/OngController.js
@@ -3,11 +3,11 @@ const connection = require('../database/connection');
 
 module.exports = {
 
-	async index(request, response) => {
+	async index(request, response) {
 		const ongs = await connection('ongs').select('*');
 
 		return response.json(ongs);
-	}
+	},
 
 	async create(request, response) {
 		const { name, email, whatsapp, city, uf } = request.body;
